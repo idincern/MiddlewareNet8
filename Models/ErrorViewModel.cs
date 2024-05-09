9ec676ff96964c6c0cdcad38dc29aa81ebcf37d8
idincern <dincernalbant@hotmail.com>
2024-05-09 11:09:23 +0300

    added mvc project to the repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiddlewareNet8.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
